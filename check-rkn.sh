ab8dd296c4170db1acb50040e5023dc478cb2d96
kvk <kostikk@gmail.com>
2019-01-30 16:26:13 +0300

    parse report.cvs

--- check-rkn.sh
+++ check-rkn.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 file="/tmp/check"
-chmod a+w $file
+>$file
 
-
-echo "Paste column URL/IP, for break empety lines"
-while read line
+#echo "Paste column URL/IP, for break empety lines"
+cat report.csv | awk -F';' '{if ($10>0) print $10; else print $6;}' > ~/report.txt
+cat ~/report.txt | while read line
 do
     # break if the line is empty
-    [ -z "$line" ] && break
-    echo "$line" | grep -Eo 'http?.://[^ >]+' >> $file
+    #[ -z "$line" ] && break
+    echo "$line" | grep -Eo 'http?.://[^ >]+'|sed 's/,\r*$//' >> $file
 done
 cat $file | wc -l
 cat $file | while read line
 do
     echo -e "\e[0m$line"
-    curl --connect-timeout 10 -vsI "$line" 2>&1 >/dev/null | grep -E "(Loca)|(Rec)|(reset)|(timed out)|(resolve)|(communicate)"
+    curl --connect-timeout 10 -vsI "$line" 2>&1 >/dev/null | grep -E "(Loca)|(Rec)|(reset)|(timed out)|(resolve)|(communicate)|(block)"
     if [ $? -ne 1 ]; then
         echo -e "\e[32mBlocked"
-    else
+     else
         echo -e "\e[31mUnblocked"
     fi
     echo " "
 done ;
-
 rm -rf $file
